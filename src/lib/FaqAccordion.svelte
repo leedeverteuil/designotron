4a4d7393e2fd528a31a7ee07a2b41da31fc9cc0b
Lee de Verteuil <deverti@protonmail.com>
2022-06-09 13:26:56 -0400

    :lipstick: Update FAQ

--- src/lib/FaqAccordion.svelte
+++ src/lib/FaqAccordion.svelte
@@ -14,11 +14,9 @@
   <button
     class="w-full flex flex-row gap-3 items-center justify-left cursor-pointer
            p-4 rounded-lg focus-outline"
-    on:click={toggle}
-  >
+    on:click={toggle}>
     <ChevronDown
-      class="text-slate-400 transition-transform {open ? 'rotate-180' : ''}"
-    />
+      class="text-slate-400 transition-transform {open ? 'rotate-180' : ''}" />
     The answers to your questions
   </button>
 
@@ -26,23 +24,28 @@
   {#if open}
     <div
       class="px-4 pb-4 border-t border-slate-400 dark:border-slate-700"
-      transition:slide={{ duration: 300 }}
-    >
+      transition:slide={{ duration: 300 }}>
       <dl class="mt-4 faq">
         <dt class="italic">Why did you make this tool?</dt>
         <dd>
-          Sometimes I feel overwhelmed by the early stages of the design process
-          and wanted a tool that could nudge me in the right direction.
+          Increase profit/enjoyment by reducing time and energy spent
+          conceptualizing page layouts.
+        </dd>
+        <br />
+
+        <dt class="italic">How frequently will content be added?</dt>
+        <dd>
+          My goal is to break down at least one website per day and dump the
+          screenshots here. Over time this will add up to a substantial
+          collection.
         </dd>
         <br />
 
         <dt class="italic">Where are the screenshots from?</dt>
         <dd>
-          Most of them are found by browsing Pinterest and finding ones that I
-          feel might fit this collection. <mark
-            >These are not my designs and I do not take credit for them!</mark
-          > I have taken care to include all the owners' names and links to the original
-          work.
+          I browse the web and break down pages that fit the collection. <mark
+            >These are not my designs and I do not take credit for them!</mark>
+          All the owners' names and links to the original work are included.
         </dd>
         <br />
 
@@ -55,14 +58,11 @@
         </dd>
         <br />
 
-        <dt class="italic">
-          What can I do with the compilation I've put together?
-        </dt>
+        <dt class="italic">What can I do with the layout I've put together?</dt>
         <dd>
-          My intention is for the compilations made here to be used purely as an
-          inspirational starting point. The designs shown here are not meant to
-          be copied pixel for pixel, but rather as a tool to kickstart one's own
-          creative work.
+          Use it as a jumping-off point for your own creative work. This tool
+          makes it easier to imagine the final product of a page by breaking
+          design choices down into small chunks.
         </dd>
       </dl>
     </div>
